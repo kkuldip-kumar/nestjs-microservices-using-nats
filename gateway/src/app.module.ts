b5e204980f3a7e3ec054f061896b82f979274881
kkuldip836 <kkuldip836@gmail.com>
2024-10-06 14:23:07 +0530

    first commit

---
+++ gateway/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'NATS_SERVICE',
+        transport: Transport.NATS,
+        options: {
+          servers: ['nats://nats']
+        }
+      }
+    ]),
+    UsersModule
+  ],
+  controllers: [
+
+  ],
+  providers: [],
+})
+export class AppModule { }
